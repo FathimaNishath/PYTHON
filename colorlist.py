76541c7c5e2b693e90675d0c0be16bbe422e54ec
FathimaNishath <149982909+FathimaNishath@users.noreply.github.com>
2023-12-05 15:49:39 +0530

    Add files via upload

---
+++ colorlist.py
@@ -0,0 +1,10 @@
+list_1=['red','blue','green']
+print("colors of list one:",list_1)
+list_2=['red','blue','green','pink']
+print("colors of list two:",list_2)
+print("colors from color_list not contained in color_list2:")
+for color in list_1:
+  continue
+else:
+    print(color)
+
